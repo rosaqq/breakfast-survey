59c772abeb242f0da765a39d0dfe397ecdc6b2e3
Tiago Rosa <tcrosa27@gmail.com>
2022-03-13 18:46:57 +0000

    Bulma was messing with GUI classes More URL variables

--- model3d.php
+++ model3d.php
@@ -27,18 +27,24 @@ foreach ($set as $res) {
             'color' => $color
     ];
 }
+
+$A_settings = [
+    'bg_color'      => $_GET['bg_color']    ?? 0xaaaaaa,    // Background color
+    'show_axes'     => $_GET['show_axes']   ?? 0,           // Show coordinate axes
+    'dot_size'      => $_GET['dot_size']    ?? 1,           // Action dots size
+    'dot_noise'     => $_GET['dot_noise']   ?? 1,           // Spheres will be placed randomly up to this*size of their original spot
+    'speed'         => $_GET['speed']       ?? 0,           // Auto rotate plane speed
+]
 ?>
 
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="media/normalize.css">
 
     <title>Breakfast Data!</title>
 </head>
@@ -47,83 +53,107 @@ foreach ($set as $res) {
         import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
         import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
         import { OBJLoader } from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/OBJLoader.js';
+        import { RoomEnvironment } from 'https://unpkg.com/three@0.127.0/examples/jsm/environments/RoomEnvironment.js';
+        import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.16/+esm';
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // SETUP
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        const renderer  = new THREE.WebGLRenderer();
-        const loader    = new OBJLoader();
-        const scene     = new THREE.Scene();
-        const camera    = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const controls  = new OrbitControls(camera, renderer.domElement);
+        const qsettings         = <?= json_encode($A_settings) ?>;
+        const loader            = new OBJLoader();
+        const gui               = new GUI();
 
-        // Initial configs
-        scene.add(camera);
-        camera.position.set(75, 50, -50);
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.outputEncoding = THREE.sRGBEncoding;
+        // Renderer
+        const renderer          = new THREE.WebGLRenderer({ antialias: true });
         renderer.domElement.id  = 'yeet';
-
-        // Insert canvas in document
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight );
         document.body.appendChild(renderer.domElement);
+
+        const pmremGenerator    = new THREE.PMREMGenerator(renderer);
+
+        // Camera
+        const camera            = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.set(75, 50, -50);
+
+        // Orbit controls
+        const orbit = new OrbitControls( camera, renderer.domElement );
+
+        // Scene configs
+        const scene             = new THREE.Scene();
+        scene.background        = new THREE.Color(qsettings.bg_color);
+        scene.environment       = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
+        scene.add(camera);
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // LIGHTS
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        let light_above = new THREE.DirectionalLight( 0xffffff, .4);
-        let light_below = new THREE.DirectionalLight( 0xffffff, .1);
-        let ambient     = new THREE.AmbientLight(0x111111);
 
-        light_above.position.set(0, 1000, 0);
-        light_below.position.set(0, -1000, 0);
-
+        let ambient             = new THREE.AmbientLight(0x000000);
         scene.add(ambient);
-        scene.add(light_above);
-        scene.add(light_below);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // GUI
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        function guiMeshStandardMaterial(gui, material) {
+            gui.add(material,'wireframe');
+            gui.add(qsettings,'speed', 0, 10).listen();
+        }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // OBJECTS
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        // Group to hold all objects for coordinated rotation
+        const plane_grp   = new THREE.Group();
+
         // Load PLANE
         let plane;
-        let speed = <?= $_GET['speed'] ?? 0 ?>; // Rotation speed from query str
         loader.load('media/plane787.obj', object => {
                 // Extract mesh from obj & set material
-                plane            = object.children[0];
-                plane.material  = new THREE.MeshStandardMaterial({color: 0x049ef4, metalness: 1, roughness: 0.4});
+                plane           = object.children[0];
+                plane.material  = new THREE.MeshStandardMaterial({color: 0x049ef4, metalness: 1, roughness: 0.8});
 
                 // Set initial properties & add to scene
                 plane.position.set(0, 0, 0);
                 plane.scale.set(1, 1, 1);
-                scene.add(plane);
+                plane_grp.add(plane);
+
+                // Add GUI settings
+                guiMeshStandardMaterial(gui, plane.material);
             }, undefined, error => console.error(error)
         );
 
-        // SPHERE
-        const sphere = new THREE.Mesh(new THREE.SphereGeometry(1, 32, 16), new THREE.MeshBasicMaterial({color: 0x00ffff}));
-        scene.add(sphere);
+        // Mouse click Sphere
+        const sphere = new THREE.Mesh(new THREE.SphereGeometry(1, 32, 16), new THREE.MeshBasicMaterial({color: 0xffff00}));
+        plane_grp.add(sphere);
 
+        // Action points
         let data        = <?= json_encode($data_points) ?>;
-        const dot_size  = 0.5;
-        const noise     = 0.1;  // variate sphere coords by this %
-        let varpos      = (val) => Math.random()*noise*val + val;
-        const sph_grp   = new THREE.Group();
+        const dot_size  = qsettings.dot_size;
+        const noise     = qsettings.dot_noise;
+
+        // positions are randomized to fall within up to <noise>% of the sphere's diameter
+        let varpos      = (x, y) => {
+            let r = noise*dot_size*2 * Math.sqrt(Math.random());
+            let a = Math.random() * 2 * Math.PI;
+            return [x + r * Math.cos(a), y + r * Math.sin(a)];
+        }
 
         data.forEach(point => {
-            let sphere = new THREE.Mesh(new THREE.SphereGeometry(dot_size, 32, 16), new THREE.MeshBasicMaterial({color: point.color}));
+            let sphere  = new THREE.Mesh(new THREE.SphereGeometry(dot_size, 32, 16), new THREE.MeshBasicMaterial({color: point.color}));
+            let [x, z]  = varpos(point.x, point.z);
 
-            sphere.position.set(varpos(point.x), point.y, varpos(point.z));
-            sph_grp.add(sphere);
+            sphere.position.set(x, point.y, z);
+            plane_grp.add(sphere);
         })
-        scene.add(sph_grp);
+
+        scene.add(plane_grp);
 
         // AXIS
-        // let axes = new THREE.AxesHelper(100);
-        // scene.add(axes);
-
-        // FOG for std background color
-        scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
+        if (qsettings.show_axes)
+            scene.add(new THREE.AxesHelper(100));
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // MOUSE INTERSECT CALCULATION
@@ -138,8 +168,7 @@ foreach ($set as $res) {
             mousePosition.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
             rayCaster.setFromCamera(mousePosition, camera);
-            let intersects = rayCaster.intersectObjects(scene.children);
-
+            let intersects = rayCaster.intersectObjects([scene.getObjectByName('787')]);
             if (intersects.length)
             {
                 let ip = intersects[0].point;
@@ -155,12 +184,11 @@ foreach ($set as $res) {
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         document.getElementById('yeet').addEventListener('dblclick', onPointerDown);
-        window.addEventListener('resize', () => {
-                camera.aspect = window.innerWidth / window.innerHeight;
-                camera.updateProjectionMatrix();
-                renderer.setSize( window.innerWidth, window.innerHeight );
-            }
-        );
+        window.addEventListener( 'resize', function () {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize( window.innerWidth, window.innerHeight );
+        }, false );
 
         function animate() {
             requestAnimationFrame(animate);
@@ -170,9 +198,8 @@ foreach ($set as $res) {
 
         function update() {
             controls.update();
-            if(plane && speed){
-                plane.rotation.y    += 0.001*speed;
-                sph_grp.rotation.y  += 0.001*speed;
+            if(plane && qsettings.speed){
+                plane_grp.rotation.y += 0.001*qsettings.speed;
             }
         }
 
